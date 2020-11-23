b89bff20261bf59cd796eda743dd7f779126aa6b
cody <cody@DESKTOP-4PD7UJ0>
2020-11-23 11:20:55 +0800

    First

---
+++ src/main/java/com/qucheng/reflectpj/service/impl/BookServiceImpl.java
@@ -0,0 +1,17 @@
+package com.qucheng.reflectpj.service.impl;
+
+import java.util.List;
+
+import com.qucheng.reflectpj.entity.Book;
+import com.qucheng.reflectpj.entity.User;
+import com.qucheng.reflectpj.service.BookService;
+
+public class BookServiceImpl implements BookService {
+
+	public void borrow(User user, List<Book> books) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
---
+++ src/main/java/com/qucheng/reflectpj/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.qucheng.reflectpj.service.impl;
+
+import com.qucheng.reflectpj.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	public void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void regist() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
